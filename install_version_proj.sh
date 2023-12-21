c2bccd3f5d0eca412be0c31e36269d4e3f187a28
Michael Sumner <mdsumner@gmail.com>
2023-12-21 06:42:37 +0000

    +x

--- install_version_proj.sh
+++ install_version_proj.sh
@@ -9,8 +9,6 @@ NCPUS=${NCPUS:-"-1"}
 
 PROJ_VERSION=${PROJ_VERSION:-"latest"}
 
-CRAN_SOURCE=${CRAN_SOURCE:-"https://cloud.r-project.org"}
-echo "options(repos = c(CRAN = '${CRAN}'))" >>"${R_HOME}/etc/Rprofile.site"
 
 # cmake does not understand "-1" as "all cpus"
 CMAKE_CORES=${NCPUS}
