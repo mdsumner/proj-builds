43618a3c4b19a8550aee95a8ba21484337405dcc
Michael Sumner <mdsumner@gmail.com>
2023-12-22 02:05:29 +0000

    descriptions

--- dockerfiles/proj_9.2.0.Dockerfile
+++ dockerfiles/proj_9.2.0.Dockerfile
@@ -3,6 +3,7 @@ FROM ubuntu:latest
 LABEL org.opencontainers.image.licenses="GPL-2.0-or-later" \
       org.opencontainers.image.source="https://github.com/mdsumner/proj-builds" \
       org.opencontainers.image.vendor="Hypertidy Project" \
+      org.opencontainers.image.description="A build of PROJ 9.2.0 for use on ubuntu" \
       org.opencontainers.image.authors="Michael Sumner <mdsumner@gmail.com>"
 
 ENV PROJ_VERSION=9.2.0
--- proj_boilerplate.R
+++ proj_boilerplate.R
@@ -39,6 +39,7 @@ docker_text <-
 LABEL org.opencontainers.image.licenses="GPL-2.0-or-later" \\
       org.opencontainers.image.source="https://github.com/mdsumner/proj-builds" \\
       org.opencontainers.image.vendor="Hypertidy Project" \\
+      org.opencontainers.image.description="A build of PROJ %s for use on ubuntu" \\
       org.opencontainers.image.authors="Michael Sumner <mdsumner@gmail.com>"
 
 ENV PROJ_VERSION=%s
@@ -48,7 +49,7 @@ COPY install_cmake_version_proj.sh /scripts/install_cmake_version_proj.sh
 RUN /scripts/install_cmake_version_proj.sh
 '
 
-tx1 <- sprintf(docker_text, label)
+tx1 <- sprintf(docker_text, label, label)
 tx2 <- sprintf(yaml_text, label, label, label, label, label, label, label, label)
 p1 <- sprintf(dockerfile_path, label)
 p2 <- sprintf(yamlfile_path, label)
